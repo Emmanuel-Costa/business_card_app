98f6f82409246eb3f519156c7de69c3d8611e885
Emmanuel-Costa <emmanuelcosta@id.uff.br>
2021-09-01 15:56:32 -0300

    Apply Room database and share content

--- app/src/main/java/br/com/example/businesscard/ui/AddBusinessCardActivity.kt
+++ app/src/main/java/br/com/example/businesscard/ui/AddBusinessCardActivity.kt
@@ -2,14 +2,37 @@ package br.com.example.businesscard.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import androidx.activity.viewModels
+import br.com.example.businesscard.App
+import br.com.example.businesscard.R
+import br.com.example.businesscard.data.BusinessCard
 import br.com.example.businesscard.databinding.ActivityAddBusinessCardBinding
 
 class AddBusinessCardActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityAddBusinessCardBinding.inflate(layoutInflater) }
+
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        val spinner: Spinner = binding.spinner
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.spinner_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+
+        }
+
         insertListeners()
     }
 
@@ -18,7 +41,26 @@ class AddBusinessCardActivity : AppCompatActivity() {
             finish()
         }
         binding.btnConfirm.setOnClickListener {
+            val businessCard = BusinessCard(
+                nome = binding.tilName.editText?.text.toString(),
+                empresa = binding.tilEmpresa.editText?.text.toString(),
+                telefone = binding.tilTelefone.editText?.text.toString(),
+                email = binding.tilEmail.editText?.text.toString(),
+                fundoPersonalizado = when(binding.spinner.selectedItem) {
+                    "Vermelho" -> "#FF0707"
+                    "Verde" -> "#1AC300"
+                    "Azul" ->"#0105FD"
+                    "Amarelo" -> "#FDFF00"
+                    "Rosa" -> "#FF00F4"
+                    "Vinho" -> "#830042"
+                    "Roxo" -> "#9B00AE"
+                    else -> "#6D6D6D"
+                }
+            )
+            mainViewModel.insert(businessCard)
+            Toast.makeText(this, R.string.label_show_success, Toast.LENGTH_LONG).show()
+            finish()
+        }
 
         }
-    }
 }
